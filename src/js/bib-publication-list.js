963e34ca336f87d5dd9e9130d40a1ab1161b5c87
Clément Aubert <caubert@augusta.edu>
2022-12-06 14:31:52 -0500

    Fixed error in inproceedings function.

--- src/js/bib-publication-list.js
+++ src/js/bib-publication-list.js
@@ -299,9 +299,8 @@ var bibtexify = (function($) {
                 ((entryData.editor) ?
                     ((lang.dir) ? lang.dir + " " : "") +
                     this.authors2html(entryData.editor) +
-                    ((lang.editor) ? lang.editor : "") : "") +
-            ". " +
-            ((entryData.publisher) ? entryData.publisher + ", " : "") +
+                    ((lang.editor) ? lang.editor + ".": "") : "") +
+                        ((entryData.publisher) ? entryData.publisher + ", " : "") +
             ((entryData.pages) ? "pp. " + entryData.pages : "") + // Not the cleanest, we assume that if there is a page number, then there is an address.
             ((entryData.address) ? ", " + entryData.address : "") +
             ".<\/em>";
